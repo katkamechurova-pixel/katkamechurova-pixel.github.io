c30277f7ee1d6f38ddee2148ec87c17bb140662e
Václav Zeman <vaclav.zeman@outlook.com>
2026-03-21 16:32:27 +0100

    sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import Sitemap from "vite-plugin-sitemap";
+import { services } from "./src/data/services";
+import { articles } from "./src/data/articles";
+
+const dynamicRoutes = [
+  ...services.map(s => `/sluzby/${s.slug}`),
+  ...articles.map(a => `/clanky/${a.slug}`)
+];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,7 +19,13 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://ducktorka.cz',
+      dynamicRoutes,
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
